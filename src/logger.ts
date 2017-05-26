2cab4119fa7772ab44e2956b8b59e01049beffd7
drioemgaoin <romain_diegoni@hotmail.com>
2017-05-26 17:07:28 +0100

    Trying to call my logger directly in my html page

--- src/logger.ts
+++ src/logger.ts
@@ -18,9 +18,10 @@ function defineLogLevel(value: number, name: string) {
   return { value: value, name: name };
 }
 
-class Logger {
+export default class Logger {
+  private static logHandler: (messages: string, context: ILoggerContext) => void;
+  private static contextualLoggersByNameMap = {};
   private context: ILoggerContext;
-  private logHandler: any;
 
   public static DEBUG = defineLogLevel(1, 'DEBUG');
   public static INFO = defineLogLevel(2, 'INFO');
@@ -33,6 +34,90 @@ class Logger {
     if (newLevel && 'value' in newLevel) {
       this.context.filterLevel = newLevel;
     }
+  }
+
+  public static setHandler(handler: (messages: string, context: ILoggerContext) => void) {
+    Logger.logHandler = handler;
+  }
+
+  public static useDefaults(options: any) {
+    console.log('EH EH');
+		Logger.setLevel(options && options.defaultLevel || Logger.DEBUG);
+		Logger.setHandler(Logger.createDefaultHandler(options));
+	}
+
+  public static setLevel(level: IFilterLevel) {
+		// Set the globalLogger's level.
+		// globalLogger.setLevel(level);
+
+		for (let key in Logger.contextualLoggersByNameMap) {
+			if (Logger.contextualLoggersByNameMap.hasOwnProperty(key)) {
+				Logger.contextualLoggersByNameMap[key].setLevel(level);
+			}
+		}
+	}
+
+  public static createDefaultHandler(options: any) {
+    options = options || {};
+
+		options.formatter = options.formatter || function defaultMessageFormatter(messages, context) {
+			if (context.name) {
+				messages.unshift("[" + context.name + "]");
+			}
+		};
+
+		let timerStartTimeByLabelMap = {};
+
+		const invokeConsoleMethod = function (hdlr, messages) {
+			Function.prototype.apply.call(hdlr, console, messages);
+		};
+
+    if (typeof console === "undefined") {
+			return function () { /* no console */ };
+		}
+
+    return function(messages, context) {
+			messages = Array.prototype.slice.call(messages);
+
+			var hdlr = console.log;
+			var timerLabel;
+
+			if (context.level === Logger.TIME) {
+				timerLabel = (context.name ? '[' + context.name + '] ' : '') + messages[0];
+
+				if (messages[1] === 'start') {
+					if (console.time) {
+						console.time(timerLabel);
+					}
+					else {
+						timerStartTimeByLabelMap[timerLabel] = new Date().getTime();
+					}
+				}
+				else {
+					if (console.timeEnd) {
+						console.timeEnd(timerLabel);
+					}
+					else {
+						invokeConsoleMethod(hdlr, [ timerLabel + ': ' +
+							(new Date().getTime() - timerStartTimeByLabelMap[timerLabel]) + 'ms' ]);
+					}
+				}
+			}
+			else {
+				if (context.level === Logger.WARN && console.warn) {
+					hdlr = console.warn;
+				} else if (context.level === Logger.ERROR && console.error) {
+					hdlr = console.error;
+				} else if (context.level === Logger.INFO && console.info) {
+					hdlr = console.info;
+				} else if (context.level === Logger.DEBUG && console.debug) {
+					hdlr = console.debug;
+				}
+
+				options.formatter(messages, context);
+				invokeConsoleMethod(hdlr, messages);
+			}
+		};
   }
 
   public getLevel(): IFilterLevel {
@@ -60,13 +145,21 @@ class Logger {
     this.invoke(Logger.ERROR, arguments);
   }
 
+  public time(label: string) {
+		if (typeof label === 'string' && label.length > 0) {
+			this.invoke(Logger.TIME, [ label, 'start' ]);
+		}
+	}
+
+  public timeEnd(label: string) {
+    if (typeof label === 'string' && label.length > 0) {
+    	this.invoke(Logger.TIME, [ label, 'end' ]);
+    }
+  }
+
   private invoke(level: IFilterLevel, msgArgs) {
-		if (this.logHandler && this.enabledFor(level)) {
-			this.logHandler(msgArgs, assign({ level: level }, {...this.context}));
+		if (Logger.logHandler && this.enabledFor(level)) {
+			Logger.logHandler(msgArgs, assign({ level: level }, {...this.context}));
 		}
 	}
 }
-
-let logger = new Logger();
-
-export default logger;
